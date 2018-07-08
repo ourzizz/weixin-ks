c6762a54ed211f286399166a2e59a960fe97bf17
ourzizz <552716481@qq.com>
2018-07-08 22:57:23 +0800

    18-7-8首页静态页面完成，服务器部署完成，前后端能通信，下阶段php和wxml通信回显

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,22 @@
 Page({
+  data: {
+  },
   clickMe: function () {
-    this.setData({ msg: "hello world!" })
+    var that = this
+    wx.request({
+      url: 'https://www.alemao.club/json/first.json',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        //将获取到的json数据，存在名字叫list的这个数组中
+        that.setData({
+          list: res.data,
+          //res代表success函数的事件对，data是固定的，list是数组
+        })
+      }
+    })
+ 
+ 
   }
 })
